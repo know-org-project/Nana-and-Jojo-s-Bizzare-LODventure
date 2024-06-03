ea93f3db0ff7552fac4bfb7dbccdf47ad039c5ff
elviraku <146867091+elviraku@users.noreply.github.com>
2024-06-03 15:57:26 +0200

    Update tei_to_rdf.py

--- Poem/tei_to_rdf.py
+++ Poem/tei_to_rdf.py
@@ -5,7 +5,7 @@ from rdflib.namespace import RDF, DC, FOAF, RDFS, DCTERMS, XSD
 TEI = Namespace("http://www.tei-c.org/ns/1.0/")
 
 # parsing the XML file
-tree = ET.parse(r'C:\Users\Эльвира\Downloads\TEI.xml')  # Adjust the path as necessary
+tree = ET.parse(r'C:\Users\Эльвира\Downloads\TEI.xml')  # adjust the path 
 root = tree.getroot()
 
 # creating an RDF graph
@@ -16,8 +16,8 @@ title = root.find('.//{http://www.tei-c.org/ns/1.0}title').text
 author = root.find('.//{http://www.tei-c.org/ns/1.0}author').text
 
 # Create URIs for resources
-poem_uri = URIRef("http://nanajojo.org/poem/ozymandias")
-author_uri = URIRef("http://nanajojo.org/poem/author/Percy_Bysshe_Shelley")
+poem_uri = URIRef("https://github.com/know-org-project/Nana-and-Jojo-s-Bizzare-LODventure/poem/ozymandias")
+author_uri = URIRef("https://github.com/know-org-project/Nana-and-Jojo-s-Bizzare-LODventure/poem/author/Percy_Bysshe_Shelley")
 
 # adding basic triples to the graph
 g.add((poem_uri, RDF.type, TEI['Poem']))
@@ -33,14 +33,14 @@ notes = [note.text for note in root.findall('.//{http://www.tei-c.org/ns/1.0}not
 isbn = root.find('.//{http://www.tei-c.org/ns/1.0}seriesStmt/{http://www.tei-c.org/ns/1.0}idno[@type="ISBN"]').text
 
 # Adding book name, edition, notes, and ISBN to the graph
-book_uri = URIRef("http://nanajojo.org/book/" + book_name.replace(" ", "_"))
+book_uri = URIRef("https://github.com/know-org-project/Nana-and-Jojo-s-Bizzare-LODventure/book/" + book_name.replace(" ", "_"))
 g.add((book_uri, DC.title, Literal(book_name)))
 g.add((poem_uri, DCTERMS.isPartOf, book_uri))  # Correcting the relationship
 g.add((book_uri, DCTERMS.hasVersion, Literal(edition)))  # Ensure edition is displayed
 g.add((book_uri, DC.identifier, Literal(isbn)))
 
 for index, note in enumerate(notes):
-    note_uri = URIRef(f"http://nanajojo.org/note/{index + 1}")
+    note_uri = URIRef(f"https://github.com/know-org-project/Nana-and-Jojo-s-Bizzare-LODventure/note/{index + 1}")
     g.add((note_uri, RDF.type, RDFS.Literal))
     g.add((note_uri, RDFS.comment, Literal(note)))
     g.add((poem_uri, DC.description, note_uri))
@@ -49,7 +49,7 @@ for index, note in enumerate(notes):
 editors = root.findall('.//{http://www.tei-c.org/ns/1.0}editor')
 for editor in editors:
     editor_name = editor.text
-    editor_uri = URIRef(f"http://nanajojo.org/editor/{editor_name.replace(' ', '_')}")
+    editor_uri = URIRef(f"https://github.com/know-org-project/Nana-and-Jojo-s-Bizzare-LODventure/editor/{editor_name.replace(' ', '_')}")
     g.add((editor_uri, RDF.type, FOAF.Person))
     g.add((editor_uri, FOAF.name, Literal(editor_name)))
     g.add((poem_uri, DC.contributor, editor_uri))
@@ -58,7 +58,7 @@ for editor in editors:
 publishers = root.findall('.//{http://www.tei-c.org/ns/1.0}publicationStmt')
 for pub in publishers:
     publisher_name = pub.find('.//{http://www.tei-c.org/ns/1.0}publisher').text
-    publisher_uri = URIRef(f"http://nanajojo.org/publisher/{publisher_name.replace(' ', '_')}")
+    publisher_uri = URIRef(f"https://github.com/know-org-project/Nana-and-Jojo-s-Bizzare-LODventure/publisher/{publisher_name.replace(' ', '_')}")
     pub_place = pub.find('.//{http://www.tei-c.org/ns/1.0}pubPlace').text
     pub_date = pub.find('.//{http://www.tei-c.org/ns/1.0}date').text
 
@@ -70,3 +70,4 @@ for pub in publishers:
 
 # the final graph
 print(g.serialize(format='turtle'))
+
